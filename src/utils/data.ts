942750593791283863076900d751afc5a4ed3b12
gunturajip <gunturaji852@gmail.com>
2025-07-25 00:16:13 +0700

    resolve font cdn and inline css, update ui

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -8,72 +8,45 @@ import testimoni6 from "../assets/images/testimoni6.webp";
 const testimonies = [
   {
     title: "Adit Firmansyah",
-    star: "⭐⭐⭐⭐⭐",
-    features: [
-      "Pelayanan Bagus",
-      "Harga Bagus",
-      "Hasil Bagus",
-      "Lancar Tanpa Masalah",
-      "Anti Ribet, Tau Beres"
-    ],
+    star: 5,
+    features: ["Pelayanan Bagus", "Harga Bagus", "Hasil Bagus", "Lancar Tanpa Masalah", "Anti Ribet, Tau Beres"],
     icon: testimoni1,
-    link: "https://www.google.com/maps/contrib/115817238128517098713/reviews?hl=en-GB"
+    link: "https://www.google.com/maps/contrib/115817238128517098713/reviews?hl=en-GB",
   },
   {
     title: "Sayuti",
-    star: "⭐⭐⭐⭐⭐",
-    features: [
-      "Berkah Selalu",
-      "Alhamdulillah Lancar",
-      "Proses Cepat",
-      "Biarkan Bintang Yang Jadi Saksi🥰"
-    ],
+    star: 5,
+    features: ["Berkah Selalu", "Alhamdulillah Lancar", "Proses Cepat", "Biarkan Bintang Yang Jadi Saksi🥰"],
     icon: testimoni2,
-    link: "https://www.google.com/maps/contrib/106242027134866729198/reviews?hl=en-GB"
+    link: "https://www.google.com/maps/contrib/106242027134866729198/reviews?hl=en-GB",
   },
   {
     title: "Dini Puspita Sari",
-    star: "⭐⭐⭐⭐⭐",
-    features: [
-      "Harga Murah",
-      "Proses Cepat",
-      "Terima Kasih, Lancar Selalu🥰"
-    ],
+    star: 5,
+    features: ["Harga Murah", "Proses Cepat", "Terima Kasih, Lancar Selalu🥰"],
     icon: testimoni3,
-    link: "https://www.google.com/maps/contrib/118315404532748638068/reviews?hl=en-GB"
+    link: "https://www.google.com/maps/contrib/118315404532748638068/reviews?hl=en-GB",
   },
   {
     title: "Nalita Alvis",
-    star: "⭐⭐⭐⭐⭐",
-    features: [
-      "Terima Kasih Banyak untuk Berkah Jasa",
-      "Sangat Amanah Sekali",
-      "Biarkan Bintangku Jadi Saksi"
-    ],
+    star: 5,
+    features: ["Terima Kasih Banyak untuk Berkah Jasa", "Sangat Amanah Sekali", "Biarkan Bintangku Jadi Saksi"],
     icon: testimoni4,
-    link: "https://www.google.com/maps/contrib/111184786605743360372/reviews?hl=en-GB"
+    link: "https://www.google.com/maps/contrib/111184786605743360372/reviews?hl=en-GB",
   },
   {
     title: "Dwi Asmanto",
-    star: "⭐⭐⭐⭐⭐",
-    features: [
-      "Proses Cepat",
-      "Gak Pakai Ribet",
-      "Lancar Terus Usahanya"
-    ],
+    star: 5,
+    features: ["Proses Cepat", "Gak Pakai Ribet", "Lancar Terus Usahanya"],
     icon: testimoni5,
-    link: "https://www.google.com/maps/contrib/103939054530389189894/reviews?hl=en-GB"
+    link: "https://www.google.com/maps/contrib/103939054530389189894/reviews?hl=en-GB",
   },
   {
     title: "Safari Umrah & Haji Travel",
-    star: "⭐⭐⭐⭐⭐",
-    features: [
-      "Harga Murah",
-      "Proses Cepat",
-      "Sukses Selalu Berkah Jasa",
-    ],
+    star: 5,
+    features: ["Harga Murah", "Proses Cepat", "Sukses Selalu Berkah Jasa"],
     icon: testimoni6,
-    link: "https://www.google.com/maps/contrib/112674763053972811979/reviews?hl=en-GB"
+    link: "https://www.google.com/maps/contrib/112674763053972811979/reviews?hl=en-GB",
   },
 ];
 
@@ -89,14 +62,14 @@ const priorities = [
       "Pendaftaran NPWP Perusahaan",
       "Sertifikat Standar (KBLI risiko menengah rendah / menengah tinggi)",
       "NIB (Nomor Induk Berusaha)",
-      "Akun OSS"
+      "Akun OSS",
     ],
     pricelist: [
       "CV wilayah Surabaya Rp2.200.000",
       "CV wilayah Sidoarjo Rp2.400.000",
       "CV wilayah Gresik Rp2.500.000",
       "CV wilayah Jawa Timur Lainnya Rp3.000.000",
-      "CV wilayah Jabodetabek Rp3.500.000"
+      "CV wilayah Jabodetabek Rp3.500.000",
     ],
   },
   {
@@ -110,14 +83,14 @@ const priorities = [
       "Pendaftaran NPWP Perusahaan",
       "Sertifikat Standar (KBLI risiko menengah rendah / menengah tinggi)",
       "NIB (Nomor Induk Berusaha)",
-      "Akun OSS"
+      "Akun OSS",
     ],
     pricelist: [
       "PT wilayah Surabaya Rp7.500.000",
       "PT wilayah Sidoarjo Rp7.500.000",
       "PT wilayah Gresik Rp7.500.000",
       "PT wilayah Jawa Timur Lainnya Rp8.000.000",
-      "PT wilayah Jabodetabek Rp8.500.000"
+      "PT wilayah Jabodetabek Rp8.500.000",
     ],
   },
   {
@@ -126,11 +99,9 @@ const priorities = [
     features: [
       "Selesai Dalam 2 Hari (Setelah Dokumen Persyaratan Benar dan Lengkap)",
       "Sertifikat Standar (KBLI risiko menengah rendah / menengah tinggi)",
-      "NIB (Nomor Induk Berusaha)"
+      "NIB (Nomor Induk Berusaha)",
     ],
-    pricelist: [
-      "NIB seluruh Indonesia Rp1.000.000",
-    ],
+    pricelist: ["NIB seluruh Indonesia Rp1.000.000"],
   },
 ];
 
@@ -151,8 +122,7 @@ const faqs = [
       "We employ a comprehensive approach to measure campaign success. This includes tracking key performance indicators (KPIs) such as engagement metrics, conversion rates, reach, and customer feedback. Our goal is to provide you with transparent and actionable insights into the effectiveness of your social media efforts.",
   },
   {
-    question:
-      "Can you give examples of your successful social media campaigns?",
+    question: "Can you give examples of your successful social media campaigns?",
     answer:
       "Certainly! We have a portfolio of successful campaigns across various industries. Check out our Case Studies to see how we've helped businesses like yours achieve their social media goals.",
   },
@@ -172,20 +142,17 @@ const faqs = [
       "We work with a wide range of social media platforms, including but not limited to Facebook, Instagram, Twitter, LinkedIn, Pinterest, and TikTok. Our strategies are adaptable to the platforms most relevant to your target audience.",
   },
   {
-    question:
-      "How often will I receive reports on the performance of my social media campaigns?",
+    question: "How often will I receive reports on the performance of my social media campaigns?",
     answer:
       "We provide regular and detailed reports on the performance of your social media campaigns. The frequency of reporting can be customized based on your preferences, with options for weekly, bi-weekly, or monthly updates.",
   },
   {
-    question:
-      "Is there ongoing support if I have questions or need adjustments to my strategy?",
+    question: "Is there ongoing support if I have questions or need adjustments to my strategy?",
     answer:
       "Absolutely. We offer ongoing support to address any questions or concerns you may have. Our team is readily available to assist you and can make adjustments to your strategy as needed to ensure it aligns with your evolving business goals.",
   },
   {
-    question:
-      "Can I see a demo or get a consultation before deciding to work with your agency?",
+    question: "Can I see a demo or get a consultation before deciding to work with your agency?",
     answer:
       "Certainly! We offer free consultations and personalized demos to showcase our approach, discuss your needs, and determine how we can best support your business. Contact us to schedule a consultation.",
   },
