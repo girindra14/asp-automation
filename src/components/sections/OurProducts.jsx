2b4d76b78a4d3d59d8bdf38a5a059907b06e03e4
gunturajip <gunturaji852@gmail.com>
2026-02-24 10:42:28 +0700

    move content to correct place

--- src/components/sections/OurProducts.jsx
+++ src/components/sections/OurProducts.jsx
@@ -17,7 +17,7 @@ import FODatasheet from "../../assets/images/our-products/fo/fo-datasheet.pdf";
 import ISG1 from "../../assets/images/our-products/isg/isg_1.png";
 import ISG2 from "../../assets/images/our-products/isg/isg_2.png";
 import ISGDatasheet from "../../assets/images/our-products/isg/isg-datasheet.pdf";
-import AICCTVDatasheet from "../../assets/images/our-products/scada/ai-cctv-datasheet.pdf";
+import AICCTVDatasheet from "../../assets/images/our-products/cctv/ai-cctv-datasheet.pdf";
 import bg from "../../assets/images/application-bg.png";
 
 export default function OurProducts({
